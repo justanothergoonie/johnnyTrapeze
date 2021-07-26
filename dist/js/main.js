97b8d6b8bdc7714c5aa14b0e97c868b03e374097
Cody J Powell <codyj.powell@outlook.com>
2021-07-26 13:02:28 -0400

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files."); // let hamburgerButton = document.querySelector('.hamburger');
+// let navList = document.querySelector('nav');
+// let closeBtn = document.querySelector('.close');
+// let toggleNav = function () {
+// 	navList.classList.toggle('showing');
+// 	hamburgerButton.classList.toggle('is-active');
+// };
+// hamburgerButton.addEventListener('click', toggleNav);
+// $(document).ready(function () {
+// 	$('.hamburger').click(function () {
+// 		$(this).toggleClass('is-active');
+// 	});
+// });
+//# sourceMappingURL=main.js.map
---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+console.log(`Hello World from main.js! 
+Change this message, and make sure it changes in the browser 
+to verify that you're working in the right files.`);
+// let hamburgerButton = document.querySelector('.hamburger');
+// let navList = document.querySelector('nav');
+// let closeBtn = document.querySelector('.close');
+
+// let toggleNav = function () {
+// 	navList.classList.toggle('showing');
+// 	hamburgerButton.classList.toggle('is-active');
+// };
+// hamburgerButton.addEventListener('click', toggleNav);
+// $(document).ready(function () {
+// 	$('.hamburger').click(function () {
+// 		$(this).toggleClass('is-active');
+// 	});
+// });
