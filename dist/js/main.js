793d2dd6b6a8795b33f2239667ddf8c566d46751
Cody J Powell <codyj.powell@outlook.com>
2021-12-05 15:15:09 -0500

    fixed the green border

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,9 @@
 "use strict";
 
-console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files."); // let hamburgerButton = document.querySelector('.hamburger');
-// let navList = document.querySelector('nav');
-// let closeBtn = document.querySelector('.close');
-// let toggleNav = function () {
-// 	navList.classList.toggle('showing');
-// 	hamburgerButton.classList.toggle('is-active');
-// };
-// hamburgerButton.addEventListener('click', toggleNav);
-// $(document).ready(function () {
-// 	$('.hamburger').click(function () {
-// 		$(this).toggleClass('is-active');
-// 	});
-// });
+console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+var box = document.querySelector('.about__text');
+var green = document.querySelector('.about__green-bar');
+var about = document.querySelector('.about__btn');
+var height = box.offsetHeight;
+green.style.height = height + 'px';
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,9 @@
 console.log(`Hello World from main.js! 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`);
-// let hamburgerButton = document.querySelector('.hamburger');
-// let navList = document.querySelector('nav');
-// let closeBtn = document.querySelector('.close');
 
-// let toggleNav = function () {
-// 	navList.classList.toggle('showing');
-// 	hamburgerButton.classList.toggle('is-active');
-// };
-// hamburgerButton.addEventListener('click', toggleNav);
-// $(document).ready(function () {
-// 	$('.hamburger').click(function () {
-// 		$(this).toggleClass('is-active');
-// 	});
-// });
+let box = document.querySelector('.about__text');
+let green = document.querySelector('.about__green-bar')
+let about = document.querySelector('.about__btn')
+let height = box.offsetHeight 
+green.style.height = height +'px'
